de4536a9bcbeb8820a70d77187e4670fd4506532
Bohdan <toyvo7@gmail.com>
2025-06-20 21:45:53 +0200

    modify loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,7 +4,7 @@ import css from "./Loader.module.css";
 export default function Loader() {
   return (
     <div className={css.loading} >
-      <DotLoader color="#48d0bc" margin={40} size={50} />
+      <DotLoader color="#48d0bc" cssOverride={{ margin: '40px auto' }} size={50} />
     </div>
   );
 }
